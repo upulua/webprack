38165459a1cce226784ddf80d58323bae4d34773
upulua <ir.borina@yandex.ru>
2023-03-20 17:15:44 +0300

    add hibernate config

--- src/main/java/ru/msu/cmc/webprack/HibernateDatabaseConfig.java
+++ src/main/java/ru/msu/cmc/webprack/HibernateDatabaseConfig.java
@@ -13,13 +13,41 @@ import java.util.Properties;
 @Configuration
 @PropertySource("classpath:application.properties")
 public class HibernateDatabaseConfig {
-    @Value("org.postgresql.Driver")
+    @Value("${driver}")
     private String DB_DRIVER;
-    @Value("jdbc:postgresql://localhost/java")
+    @Value("${url}")
     private String DB_URL;
-    @Value("postgres")
+    @Value("${username}")
     private String DB_USERNAME;
-    @Value("password")
+    @Value("${password}")
     private String DB_PASSWORD;
 
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(oraDataSource());
+        sessionFactory.setPackagesToScan("ru.msu.cmc.webprack.models");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        hibernateProperties.setProperty("connection_pool_size", "1");
+
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource oraDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(DB_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+
+        return dataSource;
+
+}
 }
